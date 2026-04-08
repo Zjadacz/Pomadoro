b21e1a6f1fb559838fef7de4c78e1fcf8a238437
Grzegorz Godlewski <jerzy.waldorff@gmail.com>
2026-04-08 14:31:25 +0200

    Initial version of project added.

---
+++ PomadoroTimer.cs
@@ -0,0 +1,74 @@
+﻿using NAudio.Wave;
+
+namespace Pomadoro
+{
+    internal class PomadoroTimer
+    {
+        public int WorkTime { get; }
+        public int BreakTime { get; }
+
+        public PomadoroTimer(int workTime, int breakTime)
+        {
+            WorkTime = workTime;
+            BreakTime = breakTime;
+        }
+
+        public async Task Start()
+        {
+            try
+            {
+                Console.CursorVisible = false;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Work phase starts!");
+                Console.Title = "Work Phase";
+
+                while (true)
+                {
+                    // Working
+                    var workTimeSpan = TimeSpan.FromMinutes(WorkTime);
+                    while (workTimeSpan > TimeSpan.Zero)
+                    {
+                        Console.Write($"\r{workTimeSpan:mm\\:ss}");
+                        await Task.Delay(1000);
+                        workTimeSpan = workTimeSpan.Subtract(TimeSpan.FromSeconds(1));
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\rFinished work phase. Break phase starts!");
+                    Console.Title = "Break Phase";
+                    PlaySound();
+
+                    // Break
+                    var breakTimeSpan = TimeSpan.FromMinutes(BreakTime);
+                    while (breakTimeSpan > TimeSpan.Zero)
+                    {
+                        Console.Write($"\r{breakTimeSpan:mm\\:ss}");
+                        await Task.Delay(1000);
+                        breakTimeSpan = breakTimeSpan.Subtract(TimeSpan.FromSeconds(1));
+                    }
+
+                    Console.WriteLine("\rFinished break. Work phase starts!");
+                    Console.Title = "Work Phase";
+                    PlaySound();
+                }
+            }
+            finally 
+            {
+                Console.CursorVisible = true;
+            }
+        }
+
+        public void PlaySound()
+        {
+            using var audioFile = new AudioFileReader("melody_alarm.wav");
+            using var outputDevice = new WaveOutEvent();
+            outputDevice.Init(audioFile);
+            outputDevice.Play();
+
+            while (outputDevice.PlaybackState == PlaybackState.Playing)
+            {
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using Pomadoro;
+using System.Collections;
+using System.Text;
+
+int workTime = 25;
+int breakTime = 5;
+
+if (args.Length > 0)
+{
+    if (!int.TryParse(args[0], out workTime))
+    {
+        DisplayHelp("Error: Work time parameter should be a simple number, f.e. '12'.");
+        return;
+    }
+}
+
+if (args.Length > 1)
+{
+    if (!int.TryParse(args[1], out breakTime))
+    {
+        DisplayHelp("Error: Break time parameter should be a simple number, f.e. '8'.");
+        return;
+    }
+}
+
+if (args.Length > 2)
+{
+    DisplayHelp("Error: Too many parameters.");
+    return;
+}
+
+var timer = new PomadoroTimer(workTime, breakTime);
+await timer.Start();
+
+static void DisplayHelp(string message = "")
+{
+    var sb = new StringBuilder();
+
+    if (!string.IsNullOrEmpty(message))
+    {
+        sb.AppendLine(message);
+        sb.AppendLine();
+    }
+
+    sb.AppendLine("Pomadoro.exe - use pomodoro technice from your command-line console.");
+    sb.AppendLine("Usage: pomodoro [worktime] [breaktime]");
+    sb.AppendLine("- worktime : (default 25 minutes) working time in simple number format, f.e. '12'.");
+    sb.AppendLine("- breaktime: (default  5 minutes) breajk time in simple number format, f.e. '2'.");
+
+    Console.WriteLine(sb.ToString());
+}
+
+
+
